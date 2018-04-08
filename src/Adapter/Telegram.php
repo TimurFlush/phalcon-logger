0faf2de788988c530e9c837ff69943ebfdce2521
Timur Flush <flush02@yandex.ru>
2018-04-09 03:04:38 +0500

    Initial

---
+++ src/Adapter/Telegram.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace TimurFlush\PhalconLogger\Adapter;
+
+use Phalcon\Logger\Adapter;
+use TimurFlush\PhalconLogger\Formatter\Telegram as TelegramFormatter;
+
+/**
+ * Class Telegram
+ * @package TimurFlush\PhalconLogger\Adapter
+ * @author TimurFlush
+ * @version 1.0.0
+ */
+class Telegram extends Adapter
+{
+    /**
+     * @var array
+     */
+    private $options = [
+        'chat_id' => '',
+        'bot_token' => '',
+        'allowed_types' => '*'
+    ];
+
+    /**
+     * Telegram constructor.
+     * @param array $options
+     */
+    public function __construct(array $options)
+    {
+        if (!isset($options['chat_id'], $options['bot_token']))
+            trigger_error("Parameters [chat_id] and [bot_token] are mandatory.");
+
+        $this->options = $options;
+    }
+
+    /**
+     * @return bool
+     */
+    public function close()
+    {
+        return true;
+    }
+
+    /**
+     * @return object|\Phalcon\Logger\FormatterInterface|TelegramFormatter
+     */
+    public function getFormatter()
+    {
+        if (!is_object($this->_formatter))
+            $this->_formatter = new TelegramFormatter();
+
+        return $this->_formatter;
+    }
+
+    /**
+     * @param string $message
+     * @param int $type
+     * @param int $time
+     * @param array $context
+     */
+    public function logInternal(string $message, int $type, int $time, array $context) : void
+    {
+        if (isset($this->options['allowed_types'])){
+            if (is_string($this->options['allowed_types']) && $this->options['allowed_types'] !== '*') {
+                return;
+            } else if (is_array($this->options['allowed_types']) && !in_array($type, $this->options['allowed_types'])) {
+                return;
+            }
+        }
+
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_CONNECTTIMEOUT => 5,
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => 'https://api.telegram.org/bot' . $this->options['bot_token'] . '/sendMessage',
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => [
+                'chat_id' => $this->options['chat_id'],
+                'text' => $this->getFormatter()->format($message, $type, $time, $context),
+                'parse_mode' => 'Markdown'
+            ]
+        ]);
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+
+}
---
+++ src/Formatter/Telegram.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace TimurFlush\PhalconLogger\Formatter;
+
+use Phalcon\Logger\Formatter;
+
+/**
+ * Class Telegram
+ * @package TimurFlush\PhalconLogger\Formatter
+ * @author TimurFlush
+ * @version 1.0.0
+ */
+class Telegram extends Formatter
+{
+    /**
+     * @var string
+     */
+    protected $_format = "\[{date}] *{type}* \n\n{message}";
+
+    /**
+     * @var string
+     */
+    protected $_dateFormat = DATE_RFC1036;
+
+    /**
+     * Telegram constructor.
+     * @param string|null $format
+     * @param string|null $dateFormat
+     */
+    public function __construct(string $format = null, string $dateFormat = null)
+    {
+        if ($format !== null)
+            $this->format = $format;
+
+        if ($dateFormat !== null)
+            $this->_dateFormat = $dateFormat;
+    }
+
+    /**
+     * @param string $message
+     * @param int $type
+     * @param int $timestamp
+     * @param null $context
+     * @return string
+     */
+    public function format($message, $type, $timestamp, $context = null)
+    {
+        $context = [
+            'date' => date($this->_dateFormat, $timestamp),
+            'type' => $this->getTypeString($type),
+            'message' => $message
+        ];
+
+        $message = $this->interpolate($this->_format, $context);
+        return $message;
+    }
+}
